a94deeaa41387705be9712e85a6868502ff87c93
rookie-leo <proleonarpa@gmail.com>
2022-11-04 07:56:49 -0300

    Utilizando estruturas condicionais

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -15,4 +15,34 @@ fun main() {
     println("Titular da conta $titular")
     println("Número da conta $numeroConta")
     println("Saldo em conta $saldo")
+
+    saldoEmContaIf(saldo)
+
+    println("Sacando valor na conta!")
+    saldo -= 250.0
+
+    saldoEmContaWhen(saldo)
+
+    println("Sacando valor na conta!")
+    saldo -= 250.0
+
+    saldoEmContaIf(saldo)
+}
+
+fun saldoEmContaIf(saldo: Double) {
+    if (saldo > 0.0) {
+        println("Saldo em conta positivo")
+    } else if (saldo == 0.0) {
+        println("Saldo em conta zerado")
+    } else {
+        println("Saldo em conta negativo")
+    }
+}
+
+fun saldoEmContaWhen(saldo: Double) {
+    when {
+        saldo > 0.0 -> println("Saldo em conta positivo")
+        saldo == 0.0 -> println("Saldo em conta zerado")
+        else -> println("Saldo em conta negativo")
+    }
 }
