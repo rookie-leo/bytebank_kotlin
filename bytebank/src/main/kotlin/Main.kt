94848a2837691e93dbc003be010e98432e823a3b
rookie-leo <proleonarpa@gmail.com>
2022-11-04 07:37:55 -0300

    Restrições das variaveis

--- bytebank/src/main/kotlin/Main.kt
+++ bytebank/src/main/kotlin/Main.kt
@@ -2,6 +2,17 @@ fun main() {
     println("Bem vindo ao ByteBank!")
 
     val titular = "Kratos"
+    val numeroConta = 1000
+    var saldo = 0.0
 
     println("Titular da conta $titular")
+    println("Número da conta $numeroConta")
+    println("Saldo em conta $saldo")
+
+    println("Adicionando valor na conta!")
+    saldo += 250.0
+
+    println("Titular da conta $titular")
+    println("Número da conta $numeroConta")
+    println("Saldo em conta $saldo")
 }
