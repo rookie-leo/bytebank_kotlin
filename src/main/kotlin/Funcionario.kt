b51c831cc1cd1bd4710e56d2674e676a7ceebea4
rookie-leo <proleonarpa@gmail.com>
2022-11-10 09:58:56 -0300

    Problema de tipos diferentes

--- src/main/kotlin/Funcionario.kt
+++ src/main/kotlin/Funcionario.kt
@@ -1,11 +1,28 @@
 package main.kotlin
 
-class Funcionario(nome: String, cpf: String) {
+class Funcionario(
+    var nome: String,
+    val cpf: String,
+    val tipo: Int//0 = analista, gerente = 1, diretor = 2
+) {
     var salario = 0.0
 
+
     fun bonificacao(): Double {
-        salario += salario * 0.10
-        return salario
+        when (tipo) {
+            0 -> {
+                salario += salario * 0.1
+                return salario
+            }
+            1 -> {
+                salario += salario * 0.2
+                return salario
+            }
+            else -> {
+                salario += salario * 0.3
+                return salario
+            }
+        }
     }
 
 }
--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,9 +3,12 @@ import main.kotlin.Funcionario
 
 fun main() {
 
-    val funcionario = Funcionario("Kratos", "01234567885")
+    val funcionario = Funcionario("Kratos", "01234567885", 2)
     funcionario.salario = 1000.00
 
-    println(funcionario.bonificacao())
+    println("Funcionário: ${funcionario.nome}")
+    println("CPF: ${funcionario.cpf}")
+    println("Salario: ${funcionario.salario}")
+    println("Salario com bonificação: ${funcionario.bonificacao()}")
 
 }
