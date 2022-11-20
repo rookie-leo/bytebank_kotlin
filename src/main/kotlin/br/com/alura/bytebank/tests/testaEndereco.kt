c5d5e626adbface8b234bd1960c351d3bf1db928
rookie-leo <proleonarpa@gmail.com>
2022-11-20 18:04:47 -0300

    Sobreescrevendo os metodos equals() e hashCode()

--- src/main/kotlin/br/com/alura/bytebank/tests/testaEndereco.kt
+++ src/main/kotlin/br/com/alura/bytebank/tests/testaEndereco.kt
@@ -4,8 +4,18 @@ import main.kotlin.br.com.alura.bytebank.models.Endereco
 
 fun testaEndereco() {
 
-    val endereco = Endereco(logradouro = "Rua dos Bobos", numero = 0, bairro = "Jardim Mary Jane")
+    val endereco = Endereco(logradouro = "Rua dos Bobos", numero = 0, bairro = "Jardim Mary Jane", cep = "00000-000")
+    val endereco2 = Endereco(logradouro = "Rua das Raposas", numero = 4, bairro = "Jardim Mary Jane", cep = "00000-000")
+    val endereco3 = Endereco(logradouro = "Rua das Raposas", numero = 4, bairro = "Jardim Mary Jane", cep = "00000-000")
 
     println(endereco)
+    println()
+    println(endereco2)
+    println()
+    println(endereco3)
+    println()
+
+    println(endereco.equals(endereco2))
+    println(endereco3.equals(endereco2))
 
 }
