4e3953c3372dd37b034f895edf65a05175431882
rookie-leo <proleonarpa@gmail.com>
2022-12-05 08:50:22 -0300

    Tratando exceptions

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -5,9 +5,42 @@ import main.kotlin.br.com.alura.bytebank.tests.testaObjeto
 
 fun main() {
 
-    println("Bem vindo ao Bytebank")
+    println("início main")
 
-    testaEndereco()
+    val entrada: String = "1.a"
+
+    val valorRecebido: Double? = try {
+        entrada.toDouble()
+    } catch (ex: NumberFormatException) {
+        println("Houve um problema na conversão do valor digitado")
+        ex.printStackTrace()
+        null
+    }
+
+    val valorComTaxa: Double? = if (valorRecebido != null) {
+        valorRecebido + 0.1
+    } else {
+        null
+    }
+
+
+
+    funcao1()
+    println("fim main")
+}
+
+fun funcao1(){
+    println("início funcao1")
+    funcao2()
+    println("fim funcao1")
+}
+
+fun funcao2() {
+    println("início funcao2")
+    for (i in 1..5){
+        println(i)
+    }
+    println("fim funcao2")
 }
 
 
