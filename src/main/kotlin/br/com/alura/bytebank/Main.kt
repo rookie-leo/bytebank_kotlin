0f3a36f758531d96d961eafaf4065b00529aa66c
rookie-leo <proleonarpa@gmail.com>
2022-12-08 08:39:01 -0300

    Testando elvis operator e safe cast

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,13 +1,20 @@
 import main.kotlin.br.com.alura.bytebank.models.Endereco
 
 fun main() {
-    val endereco: Endereco? = Endereco(logradouro = "rua dos bobos")
+    val endereco: Endereco? = Endereco(logradouro = "rua dos bobos", complemento = "casa")
     val logradouroNovo: String? = endereco?.logradouro
 
-    endereco.let { println(it?.logradouro?.length) }
+    endereco?.let {
+        println(it.logradouro?.length)
+        val tamanhoComplemento = it.complemento?.length ?: throw IllegalStateException("Complemento não pode ser nulo!")
+        println(tamanhoComplemento)
+    }
 
+    safeCast(1)
+    safeCast("")
 }
 
-
-
-
+fun safeCast(valor: Any) {
+    val numero: Int? = valor as? Int
+    println(numero)
+}
