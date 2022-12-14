404edd47463a0c8b67836a4f4d95ec05d1323d46
rookie-leo <proleonarpa@gmail.com>
2022-12-14 18:43:12 -0300

    Retornando mais de um valor na expressão lambda

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,14 +1,22 @@
-import main.kotlin.br.com.alura.bytebank.tests.minhaFuncaoAnonima
-import main.kotlin.br.com.alura.bytebank.tests.minhaFuncaoLambda
-import main.kotlin.br.com.alura.bytebank.tests.testaTipoFuncaoClasse
-import main.kotlin.br.com.alura.bytebank.tests.testaTipoFuncaoReferencia
-
 fun main() {
 //    testaTipoFuncaoReferencia()
 //    testaTipoFuncaoClasse()
 
-    println(minhaFuncaoLambda(10, 15))
-    println(minhaFuncaoAnonima(30, 45))
+//    println(minhaFuncaoLambda(10, 15))
+//    println(minhaFuncaoAnonima(30, 45))
 
+    val calculaBonificacaoLambda: (salario: Double) -> Double = lambda@ {
+        if (it > 1000.0) return@lambda it + 50
+        it + 100.0
+    }
+
+    println(calculaBonificacaoLambda(1000.0))
+
+    val calculaBonificacaoAnonima: (salario: Double) -> Double = fun(salario): Double {
+        if (salario > 1000.0) return salario + 50
+        return salario + 100.0
+    }
+
+    println(calculaBonificacaoAnonima(1001.0))
 }
 
