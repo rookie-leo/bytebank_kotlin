ce20221a85de1e8147679f2c414fe66bc0d39bbc
rookie-leo <proleonarpa@gmail.com>
2022-12-15 08:23:15 -0300

    Utilizando outras funções de escopo

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -8,7 +8,20 @@ fun main() {
             "$logradouro, $numero, $cidade".uppercase()
         }.let(::println)
 
+    val endereco = Endereco()
+        .also { println("Criando um endereço") }
+        .apply {
+            logradouro = "Largo do Paissandu"
+            numero = 13
+            cidade = "São Paulo"
+        }
+
+    with(endereco) {
+        "$logradouro, $cidade".uppercase()
+    }.let { println(it) }
+
     listOf(
+        endereco,
         Endereco(complemento = "casa",logradouro = "Av Voluntarios da Patria"),
         Endereco(logradouro = "Av Voluntarios da Patria"),
         Endereco(complemento = "Apartamento", logradouro = "Av Voluntarios da Patria"),
