16042ea91b1d53ea92433f9229c67aa59235e18c
rookie-leo <proleonarpa@gmail.com>
2022-12-15 07:22:44 -0300

    Implementando códigos com o let

--- src/main/kotlin/br/com/alura/bytebank/Main.kt
+++ src/main/kotlin/br/com/alura/bytebank/Main.kt
@@ -1,16 +1,18 @@
 import main.kotlin.br.com.alura.bytebank.models.Endereco
 
 fun main() {
-    val testeFuncao: () -> Unit
-    Endereco().let {
-        it
-    }
+    Endereco(logradouro = "Av Voluntarios da Patria", numero = 5035, cidade = "São Paulo")
+        .let {
+            "${it.logradouro}, ${it.numero}, ${it.cidade}".uppercase()
+        }.let(::println)
 
-    "world".let(::testaReceberString)
-
-    1.let { it }
+    listOf(
+        Endereco(complemento = "casa",logradouro = "Av Voluntarios da Patria"),
+        Endereco(logradouro = "Av Voluntarios da Patria"),
+        Endereco(complemento = "Apartamento", logradouro = "Av Voluntarios da Patria"),
+        Endereco(logradouro = "Av Voluntarios da Patria"),
+        Endereco(complemento = " ", logradouro = "Av Voluntarios da Patria")
+    )
+        .filter { it.complemento.isNotEmpty() && it.complemento.isNotBlank() }
+        .let(::println)
 }
-
-fun testaReceberString(str: String){ println("Hello $str") }
-
-fun teste(teste: Int, bloco: () -> Unit) {}
